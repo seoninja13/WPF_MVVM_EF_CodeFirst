b10d54cbfc717a7b15640500dadb25dcb36796f4
seoninja13 <dachevivo@gmail.com>
2019-12-06 13:44:11 -0800

    Chapter 5

--- FriendOrganizer.UI/App.xaml.cs
+++ FriendOrganizer.UI/App.xaml.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using Autofac;
+using FriendOrganizer.UI.Data;
+using FriendOrganizer.UI.StartUp;
+using FriendOrganizer.UI.ViewModel;
+using System;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
@@ -13,5 +17,14 @@ namespace FriendOrganizer.UI
     /// </summary>
     public partial class App : Application
     {
+        private void Aplication_Startup(object sender, StartupEventArgs e)
+        {
+
+            var bootstrapper = new Bootstrapper();
+            var container = bootstrapper.Bootstrap();
+
+            var mainWindow = container.Resolve<MainWindow>();
+            mainWindow.Show();
+        }
     }
 }
